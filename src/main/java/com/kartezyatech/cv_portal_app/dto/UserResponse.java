4b74a2a965fcd9c0d387037707068dd84680a130
alkan <alkan4802@gmail.com>
2022-10-02 12:59:23 +0300

    user process 2 october 12.59 fix date

--- src/main/java/com/kartezyatech/cv_portal_app/dto/UserResponse.java
+++ src/main/java/com/kartezyatech/cv_portal_app/dto/UserResponse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +14,5 @@ public class UserResponse {
     private String role;
     private String email;
     private String phoneNumber;
-    private Instant createdDate;
+    private String createdDate;
 }
